855749779f91738ef5d216c4ddba352670b67eb5
Prica <pricagamer@gmail.com>
2020-08-30 17:07:31 +0100

    package.json

--- example/main.js
+++ example/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 
 import VueLoading from '../src'
+// import VueLoading from '../lib/vue-loading-twa.min.js'
 Vue.component('VueLoading', VueLoading)
 
 Vue.config.productionTip = false
